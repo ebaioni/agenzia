a1c5394e62bc531cd8244dc9de219440acaaf26f
Enrico Baioni <baio88@gmail.com>
2014-10-26 18:35:25 +1100

    changes include

--- .openshift/themes/wpcasa/lib/admin/captcha/securimage_show.php
+++ .openshift/themes/wpcasa/lib/admin/captcha/securimage_show.php
@@ -55,7 +55,7 @@ $img = new securimage();
  * Custom captcha settings
  */
 
-require('../../../../../../wp-blog-header.php');
+require('../../../../../../../build-dependency/4.0/wp-blog-header.php');
 require_once( '../../init.php' );
 
 // Get wpSight contact fields setttings
