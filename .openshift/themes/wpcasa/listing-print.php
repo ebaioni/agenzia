2d4608458d68cf7ff0e2c2dcac224811d2584892
Enrico Baioni <baio88@gmail.com>
2015-02-02 00:18:02 +1100

    adds prezzo

--- .openshift/themes/wpcasa/listing-print.php
+++ .openshift/themes/wpcasa/listing-print.php
@@ -63,7 +63,7 @@ $listing = get_post( $listing_id ); ?>
 			<?php
 			
 				// Display listing ID
-				echo do_shortcode( '<span class="listing-id-sc">Rif.</span>[listing_id after=" --- "]' );
+				echo do_shortcode( '<span class="listing-id-sc">Rif.</span>[listing_id after=" --- Prezzo: "]' );
 				
 				// Display listing price
 				echo do_shortcode( '[listing_price]' );
