18502ce9926043e4b1265926209136b8f94091e1
Enrico Baioni <baio88@gmail.com>
2015-02-08 14:54:24 +1100

    adds aws credential

--- .openshift/config/wp-config.php
+++ .openshift/config/wp-config.php
@@ -33,6 +33,9 @@ define('DB_CHARSET', 'utf8');
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
 
+define( 'AWS_ACCESS_KEY_ID', 'AKIAIIGZKX2Y4SJIJIRQ');
+define( 'AWS_SECRET_ACCESS_KEY', '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');
+
 /**#@+
  * Authentication Unique Keys and Salts.
  *
